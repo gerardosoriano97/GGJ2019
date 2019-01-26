e1ba43c481a0f1ca8acd742f5e620684d56f8ddb
Unknown <yamilenriqueyanezmorales@hotmail.com>
2019-01-26 05:44:13 -0600

    Movimiento Oso

---
+++ Assets/Tests/YY/BearMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearMovement : MonoBehaviour
+{
+    Rigidbody body;
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraRight = Camera.main.transform.TransformDirection(Vector3.right);
+        Vector3 cameraFront = Camera.main.transform.TransformDirection(Vector3.forward); 
+        cameraRight.y = 0;
+        cameraFront.y = 0;
+        cameraRight.Normalize();
+        cameraFront.Normalize();
+        body.AddForce(cameraFront * Input.GetAxis("Vertical") * speed);
+        body.AddForce(cameraRight * Input.GetAxis("Horizontal") * speed);
+
+        transform.LookAt(transform.forward); // now just look at it. (remember that it is assumed up is Vector3.up.
+    }
+}
--- Assets/Tests/YY/CameraRotation.cs
+++ Assets/Tests/YY/CameraRotation.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CameraRotation : MonoBehaviour
 {
 
-    public float dragSpeed = 2.0f;
+    public float hSpeed = 2.0f;
+    public float vSpeed = 2.0f;
+    public float vMin = -10.0f;
+    public float vMax = 40.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +17,12 @@ public class CameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles += new Vector3(0, Input.GetAxis("RightH") * dragSpeed, 0);
+        transform.eulerAngles += new Vector3(0, Input.GetAxis("RightH") * -hSpeed, 0);
 
         float rightV = Input.GetAxis("RightV");
-        Vector3 rotation = new Vector3(rightV * dragSpeed, 0, 0);
+        Vector3 rotation = new Vector3(rightV * vSpeed, 0, 0);
         transform.localEulerAngles += rotation;
-        if (transform.localEulerAngles.x < -10.0f || transform.localEulerAngles.x > 40.0f) {
+        if (transform.localEulerAngles.x < vMin || transform.localEulerAngles.x > vMax) {
             transform.localEulerAngles -= rotation;
         }
     }
