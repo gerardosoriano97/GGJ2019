6e65e1dbe3a427c5feb0fd3b0985b42e1ff6159d
Luis Núñez <buddynod32@hotmail.com>
2019-01-26 02:06:52 -0600

    caaaambios

--- Assets/Tests/LCNG/PlayerControllerTest.cs
+++ Assets/Tests/LCNG/PlayerControllerTest.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerControllerTest : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public Vector3 goal;
+    public static PlayerControllerTest Instance { get; private set; }
+    private void Awake()
+    {
+        Instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        goal = MainTree.Instance.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(goal);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("acaacsd");
+        if (collision.gameObject.tag == "Tree")
+        {
+            goal = collision.gameObject.transform.position;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Tree")
+        {
+            goal = MainTree.Instance.transform.position;
+        }
     }
 }
---
+++ Assets/Tests/LCNG/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public static List<Tree> Items;
+    private void Awake()
+    {
+        Items.Add(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Tree")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Items.Remove(this);
+    }
+}
