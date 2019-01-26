470311dfb294bdf7a29550b367ea12ecd9279260
Luis Almaguer <imaximuz@gmail.com>
2019-01-26 15:49:27 -0600

    Amplify shader editor y ajustes en settings de graficos

---
+++ Assets/Tests/LAAG/TurnTable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTable : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+    float rotation = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation += rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        //transform.RotateAround( transform.position, Vector3.up, Time.deltaTime);
+    }
+}
