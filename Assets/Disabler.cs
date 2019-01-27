ea0e160bb04cbb38bcea884d8e64f31145914d6b
Unknown <yamilenriqueyanezmorales@hotmail.com>
2019-01-27 13:49:25 -0600

    nI IDEA

---
+++ Assets/Disabler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disabler : MonoBehaviour
+{
+    public MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
