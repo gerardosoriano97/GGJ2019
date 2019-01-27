6e1d81eed55b8a6ebd0cb2d994fe534e4fc4654a
Luis Almaguer <imaximuz@gmail.com>
2019-01-26 23:54:51 -0600

    VFX de hit y testing

---
+++ Assets/Tests/LAAG/Particles/TestSpawnLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawnLogic : MonoBehaviour
+{
+
+    public Transform target;
+    SpawnVFX spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner = GetComponent<SpawnVFX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetMouseButtonDown(0) ) {
+
+            float offsetFromCenter = 0.5f;
+            Vector3 dir = (transform.position - target.position).normalized;
+            Vector3 position = target.position + (dir * offsetFromCenter);
+
+            spawner.Spawn(position, Quaternion.LookRotation(dir));
+        }
+    }
+}
