ffa581d5216740da527ba54273f20076be131b83
Luis Almaguer <imaximuz@gmail.com>
2019-01-27 00:36:04 -0600

    Leaf falling particles and example

--- Assets/VFX/SpawnVFX.cs
+++ Assets/VFX/SpawnVFX.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class SpawnVFX : MonoBehaviour
 {
     public GameObject effectToSpawn = null;
-    //public float destroyAfter = 1.0f;
 
-    public GameObject Spawn(Vector3 position, Quaternion orientation) {
+    public GameObject Spawn(Vector3 position, Quaternion orientation, bool autoDestroyOnFinish = true) {
+        return DoSpawn(position, orientation, autoDestroyOnFinish);
+    }
+
+    public GameObject SpawnAsChild(Transform parent, Quaternion orientation, bool autoDestroyOnFinish = true) {
+        GameObject vfx = DoSpawn(parent.position, orientation, autoDestroyOnFinish);
+        vfx.transform.parent = parent;
+        return vfx;
+    }
+
+    public GameObject SpawnAsChild(Transform parent, Vector3 offset, Quaternion orientation, bool autoDestroyOnFinish = true) {
+        GameObject vfx = DoSpawn(parent.position + offset, orientation, autoDestroyOnFinish);
+        vfx.transform.parent = parent;
+        return vfx;
+    }
+
+    private GameObject DoSpawn(Vector3 position, Quaternion orientation, bool autoDestroyOnFinish = true){
         GameObject vfx = null;
         if( effectToSpawn != null ) {
             vfx = Instantiate( effectToSpawn, position, orientation);
             ParticleSystem ps = vfx.transform.GetChild(0).GetComponent<ParticleSystem>();
-            Destroy(vfx, ps.main.duration);
+            if(autoDestroyOnFinish){
+                Destroy(vfx, ps.main.duration);
+            }
         }
         else {
             Debug.Log( "No effect to spawn" );
         }
-
-        return vfx;
+        return vfx; 
     }
 
 }
