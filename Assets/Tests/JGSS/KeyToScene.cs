c4404118e76c16447565c1cad55dfed2d1c8d8e8
Gerardo Soriano <gerardosoriano97@gmail.com>
2019-01-27 15:26:34 -0600

    Add animation camera and menu

---
+++ Assets/Tests/JGSS/KeyToScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class KeyToScene : MonoBehaviour
+{
+    public string sceneToLoad;
+    public KeyCode key;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key)){
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
